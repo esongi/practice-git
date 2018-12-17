dd244c213cf5997d7311d0f1748676ac436551b1
esongi <bazel29@naver.com>
2018-12-17 23:58:59 +0900

    stairs game coding

--- exam2/src/com/bitcamp/day6/StairsGame.java
+++ exam2/src/com/bitcamp/day6/StairsGame.java
@@ -8,30 +8,52 @@ public class StairsGame {
   public static void main(String[] args) {
 
     String[] list = {"가위", "바위", "보"};
-    // 가위 > 보
-    // 보 > 바위
-    // 바위 > 가위
 
-    System.out.println("몇 계단으로 하시겠습니까: ");
+    System.out.print("몇 계단으로 하시겠습니까: ");
     Scanner sc = new Scanner(System.in);
-    int num = Integer.parseInt(sc.nextLine());
+    int goal_stairs = Integer.parseInt(sc.nextLine());
+    int my_goal = 0;
+    int com_goal = 0;
 
     while (true) {
-      System.out.println("가위,바위,보 를 입력해주세요: ");
+      System.out.print("가위,바위,보 를 입력해주세요: ");
       String my_submit = sc.nextLine();
 
       Random random = new Random();
       int com_submit = random.nextInt(list.length);
 
-      if (my_submit.equals(list[com_submit])) {
-        System.out.println("비겼습니다. 다시 입력해주세요: ");
-      } else if (list[com_submit] == "보") {
-        System.out.println("이겼습니다");
-      } else {
-        System.out.println("졌습니다");
+      if (my_submit.equals("가위") && list[com_submit].equals("보")) {
+        System.out.println("이겼습니다!");
+        my_goal += 2;
+      } else if (my_submit.equals("바위") && list[com_submit].equals("가위")) {
+        System.out.println("이겼습니다!");
+        my_goal += 3;
+      } else if (my_submit.equals("보") && list[com_submit].equals("바위")) {
+        System.out.println("이겼습니다!");
+        my_goal += 5;
+      } else if (my_submit.equals("보") && list[com_submit].equals("가위")) {
+        System.out.println("졌습니다!");
+        com_goal += 2;
+      } else if (my_submit.equals("가위") && list[com_submit].equals("바위")) {
+        System.out.println("졌습니다!");
+        com_goal += 3;
+      } else if (my_submit.equals("바위") && list[com_submit].equals("보")) {
+        System.out.println("졌습니다!");
+        com_goal += 5;
+      } else if (my_submit.equals(list[com_submit])) {
+        System.out.println(" 비겼습니다. 다시 입력해주세요: ");
       }
+      System.out.println("현재 나의 위치: " + my_goal + "계단");
+      System.out.println("컴퓨터의 위치: " + com_goal + "계단");
+      System.out.println();
 
-      sc.close();
+      if (my_goal >= goal_stairs) {
+        System.out.println("당신이 이겼습니다!");
+        return;
+      } else if (my_goal >= goal_stairs) {
+        System.out.println("컴퓨터가 이겼습니다!");
+        return;
+      }
     }
 
   }
